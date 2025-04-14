a32a4d535894e2236c5c46065b047797fb735cfe
hhhhhhhhhh1111 <48522007+hhhhhhhhhh1111@users.noreply.github.com>
2025-04-14 14:38:04 +0300

    Laba2

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -27,7 +27,6 @@ public class Startup
         services.AddSwaggerGen();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
